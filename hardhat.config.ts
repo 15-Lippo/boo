3cd516355d7bf5063cf59d8a5afe0bc0682302d3
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2022-11-29 05:30:48 +0100

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,7 +13,30 @@ module.exports = {
     //     count: 10,
     //   },
 
-    //   chainId: 1337,
+
+module.exports = {
+  networks: {
+    // ethereum: {
+    //   url: "https://rpc.flashbots.net",
+    //   accounts: {
+    //     mnemonic:
+    //       "tag palace accident hidden delay escape involve fetch mushroom corn settle doctor",
+    //     path: "m/44'/60'/0'/0",
+    //     initialIndex: 0,
+    //     count: 10,
+    //   },
+    networks: {
+    // polygon: {
+    //   url: "https://polygon-rpc.com/",
+    //   accounts: {
+    //     mnemonic:
+    //       "tag palace accident hidden delay escape involve fetch mushroom corn settle doctor",
+    //     path: "m/44'/60'/0'/0",
+    //     initialIndex: 0,
+    //     count: 10,
+    //   },
+
+     //   chainId: 1337,
     // },
     mumbai: {
       url: `https://rpc-mumbai.maticvigil.com`,
@@ -22,6 +45,24 @@ module.exports = {
     },
     harmonytest: {
       url: `https://api.s0.b.hmny.io`,
+      gas: 56000000000,
+      accounts: [`0x${process.env.HARMONY_PRIVATE_KEY}`],
+    },
+  },
+  solidity: "0.8.4",
+};
+
+export {};
+    
+    //   chainId: 137,
+    // },
+    mainnet: {
+      url: `https://polygon-rpc.com`,
+      gasPrice: 36000000000,
+      accounts: [`0x${process.env.POLYGON_PRIVATE_KEY}`],
+    },
+    harmonymainnet: {
+      url: 'https://api.harmony.one',
       gas: 56000000000,
       accounts: [`0x${process.env.HARMONY_PRIVATE_KEY}`],
     },
